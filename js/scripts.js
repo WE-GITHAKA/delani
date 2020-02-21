7dd48be80cff29a73ec9fc06c2597253da9c79f4
WE-GITHAKA <githakawendowanjiru@gmail.com>
2020-02-21 16:41:59 +0300

    Changed html file format

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,103 @@
-/ jquery for image toggle
-$(document).ready(function() {
-  $("#whatText").hide();
-  $("#whatText2").hide();
-  $("#whatText3").hide();
-  $(".imgtoggle01, .imgtoggle1").click(function() {
-    $("#whatText").toggle();
-    $(".imgtoggle01").toggle();
+$(document).ready(function(){
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-image").slideDown('1500');
+    });
   });
-  $(".imgtoggle02, .imgtoggle2").click(function() {
-    $("#whatText2").toggle();
-    $(".imgtoggle02").toggle();
+  
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
   });
-  $(".imgtoggle03, .imgtoggle3").click(function() {
-    $("#whatText3").toggle();
-    $(".imgtoggle03").toggle();
+  
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
   });
-});
-//hover effect on the portfolio section
-$(document).ready(function() {
-  $(".myportfolio").hover(
-    function() {
-      $(this)
-        .find(".myproject")
-        .show();
-    },
-    function() {
-      $(this)
-        .find(".myproject")
-        .hide();
-    }
-  );
-});
-
-//poup on submission
-$(document).ready(function() {
-  document.getElementById("form").addEventListener("submit", popUp);
-  function popUp() {
-    alert("Thanks Your message has been received.");
-  }
-});
-
- 
-function submit(){
-var name=document.getElementById("name").value;
-var messages = document.getElementById("message").value;
-if(name,messages == ''){
-    alert("Make sure all field are filled")
-}
-else{
-alert("Thank you for contacting Delani Studio.Wehave received your message" + ' ' + name );
-}
-};
+  
+  $(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $("#overlay").show();
+    }).mouseout(function(){
+      $("#overlay").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work2").mouseover(function(){
+      $("#overlay2").show();
+    }).mouseout(function(){
+      $("#overlay2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work3").mouseover(function(){
+      $("#overlay3").show();
+    }).mouseout(function(){
+      $("#overlay3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work4").mouseover(function(){
+      $("#overlay4").show();
+    }).mouseout(function(){
+      $("#overlay4").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#work5").mouseover(function(){
+      $("#overlay5").show();
+    }).mouseout(function(){
+      $("#overlay5").hide();
+    });
+    $("#work6").mouseover(function(){
+      $("#overlay6").show();
+    }).mouseout(function(){
+      $("#overlay6").hide();
+    });
+    $("#work7").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    });
+    $("#work8").mouseover(function(){
+      $("#overlay8").show();
+    }).mouseout(function(){
+      $("#overlay8").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("form#form34A").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
+  
+  
